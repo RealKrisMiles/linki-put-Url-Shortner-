15af3cc644478a8f70267073bedf5708b3ccae7d
RealKrisMiles <rajathkrishna0905@gmail.com>
2025-08-07 09:15:00 +0530

    Added RedirectController to handle URL redirections

---
+++ shortner-url-sb/shortner-url-sb/src/main/java/com/url/shortner/Controller/RedirectController.java
@@ -0,0 +1,30 @@
+package com.url.shortner.Controller;
+
+import com.url.shortner.Entity.UrlMapping;
+import com.url.shortner.Service.UrlMappingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class RedirectController {
+    private UrlMappingService urlMappingService;
+
+
+    @GetMapping("/{shorturl}")
+    public ResponseEntity<Void> redirect(@PathVariable String shorturl){
+        UrlMapping urlMapping = urlMappingService.getOriginalUrl(shorturl);
+        if(urlMapping != null){
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Location",urlMapping.getOriginalUrl());
+            return ResponseEntity.status(302).headers(httpHeaders).build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+}
