bfaf8d252989d41cfda474b28937b46b406958c4
RealKrisMiles <rajathkrishna0905@gmail.com>
2025-08-15 12:30:00 +0530

    Reviewed and checked most of the backend and authentication code

---
+++ shortner-url-sb/shortner-url-sb/src/main/java/com/url/shortner/Cors/FileConfig.java
@@ -0,0 +1,18 @@
+package com.url.shortner.Cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FileConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // <-- remove trailing slash!
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
